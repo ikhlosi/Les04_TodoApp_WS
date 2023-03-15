3d5476e568497c8db45c75b9cb2d285f01a3d764
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-03-15 09:29:31 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <View></View>
+      </View>
     </View>
   );
 }
@@ -13,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: StatusBar.currentHeight,
+  },
+  content: {
+    // backgroundColor: "red",
+    // height: 500,
+    padding: 40,
+  },
+  list: {
+    // backgroundColor: "blue",
+    // height: 100,
+    marginTop: 40,
   },
 });
